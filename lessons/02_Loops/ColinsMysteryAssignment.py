f6098ab4d2471b750caaf3c897d0b5afefb824b5
JonathanW-514 <jonathanwang514@gmail.com>
2025-05-24 01:32:58 +0000

    gcolin

--- lessons/02_Loops/ColinsMysteryAssignment.py
+++ lessons/02_Loops/ColinsMysteryAssignment.py
@@ -4,13 +4,13 @@ from cmath import phase
 import random
 from click import prompt
 from traitlets import This
-number = random.randint(1, 100)
 
 while True :
-    letter = ("B, b, C, c, D, d, F, f, G, g, H, h, J, j, K, k, L, l, M, m, N, n, P, p, Q, q, R, r, S, s, T, t, U, u, V, v, W, w, X, x, Y, y, Z, z")
-    vowel = ("a, A, e, E, i, I, o, O, u, U")
-    word = (input("GIME UR WORD MANKE SKIBIDI RIZZ GIDAGAGAGIDEO "))
-    print(number)
-    if word == letter:
-        word = word - letter + letter
-    
+    ay = "ay"
+    consonants = "B, b, C, c, D, d, F, f, G, g, H, h, J, j, K, k, L, l, M, m, N, n, P, p, Q, q, R, r, S, s, T, t, U, u, V, v, W, w, X, x, Y, y, Z, z"
+    vowel = "a, A, e, E, i, I, o, O, u, U"
+    words = input(str("GIME UR WORD MANKE SKIBIDI RIZZ GIGAGAGAGIDIEO NO CAP FR FR"))
+    if input:
+        for word in words.split():
+            print(word[1:] + "-" + word[0] + "ay")
+        
